428ecc592ff5e104a9bcac1b64df1688ff469602
bobby <bobby@tokodistributor.com>
2019-07-22 13:26:42 +0700

    fix bug

--- src/main/java/message/service/services/ProductListener.java
+++ src/main/java/message/service/services/ProductListener.java
@@ -14,7 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-@KafkaListener(offsetReset = OffsetReset.EARLIEST)
+@KafkaListener(batch = true)
 public class ProductListener {
     private final ProductRepository productRepository;
 
